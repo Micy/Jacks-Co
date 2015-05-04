744aaf057ff308e493e7dba7ee7a6d7c33cbf82c
Micy <michael.clayson@btinternet.com>
2015-05-04 10:10:36 +0100

    Emailer Update

--- SourceCode/Emailer.java
+++ SourceCode/Emailer.java
@@ -10,29 +10,7 @@ import java.util.*;
 
 public class Emailer {
 
-	String emailTo;
-	private final static String FROMADDRESS = "support@jacks-co.com"; 
-
-
-
-
-	
-
-
-	public void sendEmail() {
-		SendGrid sendgrid = new SendGrid("jacks-co", "michaelclayson1");
-		SendGrid.Email email = new SendGrid.Email();
-		email.addTo(emailTo);
-		email.setFrom(FROMADDRESS);
-		email.setSubject("YoYoYo");
-		email.setText("sent from a class within a class baby!");
-		try {
-			SendGrid.Response response = sendgrid.send(email);
-			System.out.println(response.getMessage());
-		} catch (SendGridException e) {
-			System.err.println(e);
-		}
-	}
+	private final static String FROMADDRESS = "Support@jacks-co.me"; 
 
 	public static boolean ticketAccepted(Ticket ticket, User user){
 		SendGrid sendgrid = new SendGrid("jacks-co", "michaelclayson1");
@@ -42,13 +20,13 @@ public class Emailer {
 		
 		email.addTo(user.getEmail());
 		email.setFrom(FROMADDRESS);
-		email.setSubject("Ticket - "+ticket.getTicketID()+" Has been accepted");
-		email.setText(	"Dear "+user.getTitle()+" "+user.getForename()+" "+user.getSurname()+"\n\n"+
-						"Your Ticket - "+ticket.getTicketID()+" has been accepted by a support engineer.\n"+
-						"Your allocated engineer is "+engineer.getForename()+" "+engineer.getSurname()+"\n"+
+		email.setSubject("Ticket - "+ticket.getTicketID()+" has been accepted");
+		email.setText(	"Dear "+user.getTitle()+" "+user.getForename()+" "+user.getSurname()+",\n\n"+
+						"Your Ticket - "+ticket.getTicketID()+" has been accepted by a Support Engineer.\n"+
+						"Your allocated Engineer is "+engineer.getForename()+" "+engineer.getSurname()+"\n"+
 						"Email - "+engineer.getEmail()+"\n"+
 						"Telephone - "+engineer.getPhoneNumber()+"\n"+
-						"Your Ticket will be Completed as soon as possible\n\n"+
+						"Your Ticket will be completed as soon as possible\n\n"+
 						"Thank you for using Jacks-Co Support!"
 			);
 		try {
@@ -71,13 +49,13 @@ public class Emailer {
 		
 		email.addTo(user.getEmail());
 		email.setFrom(FROMADDRESS);
-		email.setSubject("Ticket - "+ticket.getTicketID()+" Has been been Completed!");
-		email.setText(	"Dear "+user.getTitle()+" "+user.getForename()+" "+user.getSurname()+"\n\n"+
-						"Your Ticket - "+ticket.getTicketID()+" has been been marked as completed.\n"+
-						"Your allocated engineer was "+engineer.getForename()+" "+engineer.getSurname()+"\n"+
+		email.setSubject("Ticket - "+ticket.getTicketID()+" has been completed!");
+		email.setText(	"Dear "+user.getTitle()+" "+user.getForename()+" "+user.getSurname()+",\n\n"+
+						"Your Ticket - "+ticket.getTicketID()+" has been marked as completed.\n"+
+						"Your allocated Engineer was "+engineer.getForename()+" "+engineer.getSurname()+"\n"+
 						"Email - "+engineer.getEmail()+"\n"+
 						"Telephone - "+engineer.getPhoneNumber()+"\n"+
-						"If your issue has not been resolved do not hesitate to contact the above support engineer for further assistance\n\n"+
+						"If your issue has not been resolved do not hesitate to contact the above Support Engineer for further assistance\n\n"+
 						"Thank you for using Jacks-Co Support!"
 			);
 		try {
